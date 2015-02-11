2ac73568a45cb6f81b5af8b23dbb2a5402c9e2e7
Kristonitas <Kristupas.stumbrys@gmail.com>
2015-02-11 23:14:31 +0200

    expanded user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  # remove excess spaces in name
+  before_save {self.name = name.squish}
+
+  # validations for different fields
+  validates :name, presence: true, length: {maximum: 50}
+  validates :password, length: {minimum: 6}
+
+  # method that forces having a  password
+  has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "Example User", password: "foobar",
+                password_confirmation: "foobar")
+  end
+
+  test "name should be squished" do
+    @user.name = "   John   Smith   "
+    @user.save
+    assert @user.name == "John Smith"
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
 end
