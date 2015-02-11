ef8ae628364c97b1b30a2bc34f689efcf8d4c725
Kristonitas <Kristupas.stumbrys@gmail.com>
2015-02-11 23:37:27 +0200

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211212540) do
+ActiveRecord::Schema.define(version: 20150211213635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attempts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "booth_id"
+    t.integer  "score"
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "booths", force: :cascade do |t|
     t.string   "name"
