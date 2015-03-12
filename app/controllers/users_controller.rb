b2ac6ecd762ba544e77110bcbf6c11764dc39885
Kristonitas <Kristupas.stumbrys@gmail.com>
2015-03-12 18:50:23 +0200

    tmp qr codes added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,6 @@
+require 'rqrcode'
+require 'rqrcode_png'
+
 class UsersController < ApplicationController
   skip_before_filter :verify_authenticity_token, :except => :create
   protect_from_forgery :except => :create
@@ -18,6 +21,10 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        qr = RQRCode::QRCode.new( "http://79.98.25.158/attempt_for/#{@user.id}", :size => 3, :level => :l )
+        png = qr.to_img
+        png.resize(256, 256).save("public/qr_codes/#{@user.id}_qr.png")
+
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, location: @user }
       else
