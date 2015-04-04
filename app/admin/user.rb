fa377354e6a35cf5431206ae9144e95590cf226c
Kristonitas <Kristupas.stumbrys@gmail.com>
2015-04-04 00:35:14 -0500

    active admin with old users created

---
+++ app/admin/user.rb
@@ -0,0 +1,23 @@
+ActiveAdmin.register User do
+  permit_params :name, :password, :password_confirmation
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :booth_id
+    actions
+  end
+
+  filter :name
+
+  form do |f|
+    f.inputs "Fidi User Details" do
+      f.input :name
+      f.input :password 
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+
+end
