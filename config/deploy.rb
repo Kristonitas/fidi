3514355e743b2bfe6ec9e7cda592c08b1d337d41
Kristonitas <Kristupas.stumbrys@gmail.com>
2015-04-09 12:04:17 -0500

    deploy config changed, server should restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,3 +41,16 @@ set :keep_releases, 5
 # namespace :deploy do
 #   after 'deploy:setup_config', 'nginx:reload'
 # end
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+  
+  after :publishing, :restart
+  
+end
