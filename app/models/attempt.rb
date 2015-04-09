0adafac422892d89f8a4f3b736a6240d33e90f19
Kristonitas <Kristupas.stumbrys@gmail.com>
2015-04-09 12:51:31 -0500

    is_record set automatically

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -5,4 +5,14 @@ class Attempt < ActiveRecord::Base
   validates :score, presence: true
   validates_presence_of :user
   validates_presence_of :booth
+
+  before_save do
+  	last_record = self.user.best_attempts.where(booth_id: self.booth_id, is_record:true ).first
+  	if last_record.nil?
+  		self.is_record = true
+  	elsif last_record.score < self.score
+  		self.is_record = true
+  	end
+  end
+
 end
